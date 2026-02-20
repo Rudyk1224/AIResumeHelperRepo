49ad0ef8eb8c524550c28607a52be4376bea2288
Rudyk1224 <138708140+Rudyk1224@users.noreply.github.com>
2026-02-20 15:02:02 -0600

    added initial project

---
+++ AIResumeProject/AIResumeProject/Backend/PDFExtractorService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIResumeProject.Backend
+{
+    public class PDFExtractorService
+    {
+        public void ExtractTextFromPDF(MemoryStream stream)
+        {
+            if(stream == null)
+            {
+                throw new ArgumentException("Invalid or empty PDF");
+            }
+        }
+    }
+}
---
+++ AIResumeProject/AIResumeProject/HTTPResumeWatcher.cs
@@ -0,0 +1,34 @@
+using AIResumeProject.Backend;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace AIResumeProject
+{
+    public class HTTPResumeWatcher
+    {
+        private readonly ILogger<HTTPResumeWatcher> _logger;
+        private readonly PDFExtractorService pdfExtractorService;
+
+        public HTTPResumeWatcher(ILogger<HTTPResumeWatcher> logger, PDFExtractorService pdfService)
+        {
+            _logger = logger;
+            pdfExtractorService = pdfService;
+        }
+
+        [Function("UploadResume")]
+        public async Task<IActionResult> HandleResumePDF([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request. Handling Resume PDF");
+            using var memoryStream = new MemoryStream();
+            await req.Body.CopyToAsync(memoryStream); //copy the request's body containing the data (or PDF bytes) to the blank memory stream
+            memoryStream.Position = 0; //reset the position of the pointer to the start of the PDF bytes
+
+            pdfExtractorService.ExtractTextFromPDF(memoryStream);
+
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
